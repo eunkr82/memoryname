ebb3dd9b7c966017f1609f8b3bac30b9f69876f7
eunkr82 <eun.kr82@gmail.com>
2024-10-21 22:50:19 +0900

    fix

--- src/pages/GuestBook.jsx
+++ src/pages/GuestBook.jsx
@@ -5,10 +5,11 @@ const GuestBook = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    axios.get("https://2024-ewdm.site/guestbook")  // 전체 URL 경로로 수정
+    axios.get("https://2024-ewdm.site/guestbook")  
       .then(response => setPosts(response.data))
       .catch(error => console.error('Error fetching guestbook posts:', error));
-  }, []);  // 빈 배열을 넣어야 useEffect가 컴포넌트 마운트 시에만 한 번 실행됨
+  }, []);  
+  
 
   return(
     <div>{posts.length}</div>
